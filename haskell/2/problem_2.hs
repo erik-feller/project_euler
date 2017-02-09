68fd06f7e2ad4880563cafd0f4f0bdc4302c858d
erik-feller <erik.feller@colorado.edu>
2017-02-09 11:45:08 -0700

    Solved problem 2

--- haskell/2/problem_2.hs
+++ haskell/2/problem_2.hs
@@ -1,4 +1,6 @@
 fibSum :: (Integral a) => a -> a -> a -> a
 fibSum firstVal secondVal limit
-    | (firstVal + secondVal) > limit = secondVal
-    | otherwise = secondVal + fibSum secondVal (firstVal+secondVal) limit
+    | ((firstVal + secondVal) > limit) && (secondVal `mod` 2 == 0) = secondVal
+    | ((firstVal + secondVal) > limit) && (secondVal `mod` 2 /= 0) = 0
+    | secondVal `mod` 2 == 0 = secondVal + fibSum secondVal (firstVal+secondVal) limit
+    | otherwise = fibSum secondVal (firstVal+secondVal) limit
