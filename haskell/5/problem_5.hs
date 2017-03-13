63112de4396c4045cfa5c33094a8cd287d0e3c8f
Erik Feller <erik.feller@colorado.edu>
2017-03-13 14:25:03 +0000

    This works but is slow. Real slow

--- haskell/5/problem_5.hs
+++ haskell/5/problem_5.hs
@@ -1,1 +1,3 @@
---[z | z <- [1000000..10000000], (sum [x `mod` y | x <- [z], y <- [1..20]]) == 0] This should work as you adjust the limits looking for better
+[z | z <- [1000000..10000000], (sum [x `mod` y | x <- [z], y <- [1..20]]) == 0] This should work as you adjust the limits looking for better
+--[232792560,465585120,698377680,931170240]
+--Solution set provided proper bounds
